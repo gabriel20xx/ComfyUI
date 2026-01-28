ed4760761b8b5c3d14284c2b57abd63adf6c1307
Martial Michel <7586284+mmartial@users.noreply.github.com>
2026-01-27 21:13:14 -0500

    1.2.1 release

--- userscripts_dir/13-nunchaku.sh
+++ userscripts_dir/13-nunchaku.sh
@@ -4,7 +4,7 @@
 # - 00-nvidiaDev.sh
 
 # https://github.com/nunchaku-tech/nunchaku
-nunchaku_version="v1.2.0"
+nunchaku_version="v1.2.1"
 
 # detects the compute capability of the GPUs present on the machine and compiles only for those SMs
 export NUNCHAKU_INSTALL_MODE=FAST
